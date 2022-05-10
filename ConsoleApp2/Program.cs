5eccca1dd3c4ba7474beae77fbced706989847c7
DELL <DELL@DELL>
2022-05-10 14:57:57 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = 9;
+            b = 15;
+            c = a & b;
+            Console.WriteLine("c=" + c);
+            
+
+            c = a|b;
+            Console.WriteLine("c=" + c);
+     
+
+            c = a^b;
+            Console.WriteLine("c=" + c);
+         
+            
+            c = a<<1;
+            Console.WriteLine("c=" + c);
+          
+
+            c = a>>1
+                ;
+            Console.WriteLine("c=" + c);
+
+            int x = 100;
+            x+=10;
+            Console.WriteLine("x=" +x);
+
+
+            int y = 24;
+            y-= 10;
+            Console.WriteLine("y="+y);
+
+            int d= 25;
+            d= 24*d;
+            Console.WriteLine("d="+d);
+
+            int v = 100;
+            Console.WriteLine("v="+(++v));
+            v = v / 2;
+            Console.WriteLine("v=" + v);
+
+            int
+
+
+
+
+            Console.Read();
+
+
+
+        }
+
+      
+
+
+
+
+
+    }
+}
---
+++ myConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("HELLO WORLD");
+            Console.WriteLine("enter any message");
+            string msg = Console.ReadLine();
+            Console.WriteLine("our message is " + msg);
+            
+
+            int id = 101;
+         /*   string name = "naman";
+            float income = 19000.99f;
+            double cost = 1.9E2;
+            char gender = 'M';*/
+
+            Console.WriteLine("id="+id);
+            Console.Read();
+
+
+         
+
+        }
+    }
+}
